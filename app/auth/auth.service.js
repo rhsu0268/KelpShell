430cf1e9f37d4eae7170ffdc76bcad340303633a
Richard Hsu <rhsu@gwu.edu>
2016-05-28 16:50:35 -0400

    Cleaned repo

--- app/auth/auth.service.js
+++
@@ -1,9 +0,0 @@
-angular.module('angularfireSlackApp').factory('Auth', function($firebaseAuth, FirebaseUrl) {
-
-	var ref = new Firebase(FirebaseUrl);
-	var auth = $firebaseAuth(ref);
-
-	return auth;
-
-
-});
