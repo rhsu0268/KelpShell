c071768ae5443106b2d48a370025bd3956d2e731
Richard Hsu <rhsu@gwu.edu>
2016-05-28 14:47:27 -0400

    Pulling in login and register pages

--- app/app.js
+++ app/app.js
@@ -31,4 +31,4 @@ angular
 
     $urlRouterProvider.otherwise('/');
   })
-  .constant('FirebaseUrl', 'https://slack.firebaseio.com/');
+  .constant('FirebaseUrl', 'https://kelpa-61adc.firebaseio.com');
