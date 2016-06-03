3beb69e4435f4c27d83346aaa4b23bb63d8a9f3c
Richard Hsu <rhsu@gwu.edu>
2016-06-02 20:02:36 -0400

    Pulled in Firebase

--- Kelpa/routes/index.js
+++ Kelpa/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/connect_to_firebase', function(req, res, next) {
+
+});
+
 module.exports = router;
