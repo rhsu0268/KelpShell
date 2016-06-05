38e0088427494685f9095466e3f327ab8dbe67b9
Richard Hsu <rhsu@gwu.edu>
2016-06-04 22:15:57 -0400

    Added Music Mixer page

--- Kelpa/routes/index.js
+++ Kelpa/routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
 
 router.get('/connect_to_firebase', function(req, res, next) {
 
+});
+
+router.get('/music_mixer', function(req, res, next) {
+	res.render('music_player', { title: 'Express' });
 });
 
 module.exports = router;
