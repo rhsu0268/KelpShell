a1a12284bacd65885865899ac1084a3e478037ec
Richard Hsu <rhsu@gwu.edu>
2016-06-07 21:54:19 -0400

    Added pitch detecotr

--- public/javascripts/music_mixer.js
+++ public/javascripts/music_mixer.js
@@ -1,23 +1,40 @@
 var app = angular.module('musicMixer', []);
 
+var context = new AudioContext();
+var osc = context.createOscillator();
 
-app.controller('musicMixer', function() {
+app.controller('musicMixer', ['$scope', function($scope) {
 
 	console.log("music mixer");
 
-	context = new AudioContext();
-	console.log(context);
 
 
-	osc = context.createOscillator();
-	console.log(osc);
-	osc.frequency.value = 550;
+	$scope.generatePitch300 = function()
+	{
+		generatePitch(300);
+		osc.start(0);
+	}
+
+	$scope.generatePitch350 = function()
+	{
+		generatePitch(350);
+		osc.start(0);
+	}
+
+	$scope.stopPitch = function()
+	{
+		osc.stop();
+	}
+}]);
+
+function generatePitch(pitch)
+{
+	//console.log(context);
+	//console.log(osc);
+	osc.frequency.value = pitch;
 	osc.connect(context.destination);
+}
 
-	osc.start(0);
 
-	osc.stop();
-
-});
 
 
