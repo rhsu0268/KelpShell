c7eae87cf070b14568f6f859cb52c3edb13d981d
Richard Hsu <rhsu@gwu.edu>
2016-06-19 17:33:24 -0400

    Fixed navbar on homepage

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' });
+	res.render('index', { title: 'KelpShell' });
 });
 
 router.get('/connect_to_firebase', function(req, res, next) {
