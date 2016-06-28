ed60236578bffb408c30c2fee5488445182ce485
Richard Hsu <rhsu0268@gmail.com>
2016-06-27 21:09:55 -0400

    Tied function to play music on the pad button

--- public/javascripts/composer.js
+++ public/javascripts/composer.js
@@ -1,9 +1,39 @@
 var app = angular.module('composer', []);
 
+var context;
+var osc;
+var source;
+var gain;
 
 app.controller('padsCtrl', ['$scope', function($scope) {
 
     console.log("Pads");
 
+    $scope.playKick = function()
+    {
+        context = new AudioContext();
+		bufferLoader = new BufferLoader(
+	    	context,
+	    	[
+		      '../music/feeling_good.mp3'
+		    ],
+		    finishedLoading	// this is the callback function - it's called after the file is loaded
+		    				// and is given an array of loaded buffer arrays as an argument
+		);
+
+	  	bufferLoader.load();
+    }
+
 
 }]);
+
+function finishedLoading(bufferList) {
+ 	// If you had more loops, you could
+  	source = context.createBufferSource();
+  	source.buffer = bufferList[0];
+  	gain = context.createGain();
+  	source.connect(gain);
+  	gain.gain.value = 0.4;
+  	gain.connect(context.destination);
+  	source.start(0);
+}
