2a3addd3d26eb8d87e9bd9af65b620e68adf1748
Richard Hsu <rhsu0268@gmail.com>
2016-06-29 00:06:29 -0400

    Added loop for sound

--- public/javascripts/composer.js
+++ public/javascripts/composer.js
@@ -71,6 +71,13 @@ app.controller('padsCtrl', ['$scope', function($scope) {
         }
     }
 
+    $scope.playLoop1 = function()
+    {
+        console.log("Play loop1");
+        playSoundLoop("kick.wav");
+
+    }
+
 
 }]);
 
@@ -99,4 +106,33 @@ function finishedLoading(bufferList) {
   	gain.gain.value = 0.4;
   	gain.connect(context.destination);
   	source.start(0);
+}
+
+function playSoundLoop(tune)
+{
+    context = new AudioContext();
+    bufferLoader = new BufferLoader(
+        context,
+        [
+          '../music/' + tune
+        ],
+        finishedLoadingAndPlay	// this is the callback function - it's called after the file is loaded
+                        // and is given an array of loaded buffer arrays as an argument
+    );
+
+    bufferLoader.load();
+}
+
+function finishedLoadingAndPlay(bufferList) {
+    // If you had more loops, you could
+    source = context.createBufferSource();
+    source.buffer = bufferList[0];
+    gain = context.createGain();
+    source.connect(gain);
+    gain.gain.value = 0.4;
+    gain.connect(context.destination);
+
+  	source.start(0);
+    source.loop = true;
+
 }
