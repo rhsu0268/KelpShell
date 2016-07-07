4e6c6ac8af619c145b4b5239e5fd11982f9e16af
Richard Hsu <rhsu0268@gmail.com>
2016-07-06 21:47:46 -0400

    Pulled in angular on extravagantPieces

--- public/javascripts/extravagantPieces.js
+++ public/javascripts/extravagantPieces.js
@@ -3,5 +3,6 @@ var app = angular.module('extravagantPieces', []);
 
 function KelpShell(fbname)
 {
+	// create firebase reference
 	this.firebase = new Firebase("https://" + fbname + ".firebaseio.com/");
 }
