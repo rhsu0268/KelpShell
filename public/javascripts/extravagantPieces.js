d0b502d91ae7fdfd06c929b32a4a979f63a082d6
Richard Hsu <rhsu0268@gmail.com>
2016-07-07 22:00:54 -0400

    Switched over to angular pullin

--- public/javascripts/extravagantPieces.js
+++ public/javascripts/extravagantPieces.js
@@ -1,6 +1,6 @@
-var app = angular.module('extravagantPieces', []);
+var app = angular.module('extravagantPieces', ['firebase']);
 
-
+/*
 function KelpShell(fbname)
 {
 	// create firebase reference
@@ -12,18 +12,50 @@ function KelpShell(fbname)
 
 	this.submitPiece = function(composer, title)
 	{
-		piecesRef.child(btoa(composer)).set({
-			title: title
+		console.log("composer" + composer);
+		console.log("title" + title);
+
+		piecesRef.child().set({
+			title: title,
+			composer: composer
 
 		});
-	}
+	};
+
+	this.onPiecesChanged = function() {};
+
+	piecesRef.on('value', function(snapshot) {
+		var pieces = snapshot.val();
+		console.log(pieces);
+		var preparedPieces = [];
+
+		for (var title in pieces)
+		{
+			if (pieces.hasOwnProperty(title))
+			{
+				preparedPieces.push({
+					title: pieces[title].title
+				})
+			}
+		}
+		console.log(preparedPieces);
+		this.onPiecesChanged(preparedPieces);
+	}.bind(this));
+
 };
+*/
 
 
-app.controller('sharePiecesCtrl', ['$scope', function($scope) {
 
-	var pieces = new KelpShell('kelpshell');
 
+app.controller('sharePiecesCtrl', ['$scope', '$compile', '$firebaseArray', function($scope, $compile, $firebaseArray) {
+
+	var pieces = new Firebase('https://kelpshell.firebaseio.com/');
+	//$scope.masterpieces = pieces.onPiecesChanged;
+
+	$scope.masterpieces = $firebaseArray(pieces)
+
+	/*
 	$scope.sharePiece = function()
 	{
 		//pieces.submitPiece();
@@ -36,4 +68,11 @@ app.controller('sharePiecesCtrl', ['$scope', function($scope) {
 		$scope.composer = "";
 	}
 
+	pieces.onPiecesChanged = function(titles)
+	{
+		console.log(titles);
+		$scope.masterpieces = titles;
+		console.log($scope.masterPieces);
+	}
+	*/
 }]);
