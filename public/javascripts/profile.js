aa48087637ec58c31351f629a182ea5c9ce0e467
Richard Hsu <rhsu0268@gmail.com>
2016-07-17 15:26:07 -0400

    Added profile object code

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,1 +1,21 @@
-var app = angular.module('profile', []);
+var app = angular.module('profile', ["firebase"]);
+
+app.factory("Profile", ["$firebaseObject", function($firebaseObject) {
+
+    return function(username)
+    {
+        // create a reference to the database node where we will store our data
+        var ref = firebase.database().ref("rooms").push();
+        var profileRef = ref.child(username);
+
+        // return it as a synchronized object
+        return $firebaseObject(profileRef);
+    }
+
+}]);
+
+app.controller("ProfileCtrl", ["$scope", "Profile", function($scope, Profile) {
+
+    Profile("richardhsu").$bindTo($scope, "profile");
+
+}]);
