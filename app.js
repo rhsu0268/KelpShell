3596fde6a06d9d44d16c08e91bf2bc2496b929d6
Richard Hsu <rhsu0268@gmail.com>
2016-07-24 22:36:07 -0400

    Added a model for users

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/kelpshell');
+
 var app = express();
 
 // view engine setup
