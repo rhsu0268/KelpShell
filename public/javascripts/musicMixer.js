abcc5e4973b480b9ec40530db7e3c81566300867
Richard Hsu <rhsu0268@gmail.com>
2016-08-13 16:15:03 -0400

    Reformatted mixer page

--- public/javascripts/musicMixer.js
+++ public/javascripts/musicMixer.js
@@ -92,42 +92,6 @@ app.controller('musicMixer', ['$scope', function($scope) {
 	//var osc = context.createOscillator();
 
 
-
-	$scope.generatePitch300 = function()
-	{
-		//osc.stop();
-
-		generatePitch(300);
-		osc.start(0);
-	};
-
-	$scope.generatePitch350 = function()
-	{
-		//osc.stop();
-
-		generatePitch(350);
-		osc.start(0);
-	};
-
-	$scope.stopPitch = function()
-	{
-		if (osc)
-		{
-			osc.disconnect(context.destination);
-			osc.stop();
-		}
-
-	};
-
-
-	$scope.startPitch = function()
-	{
-		context = new AudioContext();
-		osc = context.createOscillator()
-		osc.connect(context.destination);
-		osc.start(0);
-	};
-
 	$scope.slider = {
   		value: 150,
   		options: {
@@ -166,55 +130,8 @@ app.controller('musicMixer', ['$scope', function($scope) {
 }]);
 
 
-app.controller('bandJS', function($scope) {
-
-	console.log("Inside bandjs");
-
-	$scope.tempo = 180;
-
-	var conductor = new BandJS();
-
-	conductor.setTimeSignature(2, 2);
-	conductor.setTempo($scope.tempo);
-
-	var rightHand = conductor.createInstrument('square', 'oscillators');
-	var leftHand = conductor.createInstrument('triangle', 'oscillators');
-	var drum = conductor.createInstrument('white', 'noises');
-
-	drum.setVolume(50);
-
-	// Bar 1
-	rightHand.note('quarter', 'E5, F#4');
-	rightHand.note('quarter', 'E5, F#4');
-	rightHand.rest('quarter');
-	rightHand.note('quarter', 'E5, F#4');
 
 
-	$scope.playMixedSong = function()
-	{
-		$scope.playing = true;
-        $scope.paused = false;
-        player.play();
-	}
-
-	var player = conductor.finish();
-
-	$scope.stopMixedSong = function()
-	{
-		$scope.playing = false;
-		$scope.paused = false;
-		player.pause();
-	}
-
-});
-
-function generatePitch(pitch)
-{
-	//console.log(context);
-	//console.log(osc);
-	osc.frequency.value = pitch;
-	//osc.connect(context.destination);
-}
 
 function finishedLoading(bufferList) {
  	// If you had more loops, you could
@@ -261,16 +178,7 @@ app.controller('mixer', function($scope) {
 
 });
 
-app.controller('pad', function($scope) {
 
-	console.log("Pad");
-
-	var context = new AudioContext();
-
-
-
-
-});
 
 
 // function to load audio files
