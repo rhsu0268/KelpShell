22a93e627c2345b39c852dc68be4729d7e01e702
Richard Hsu <rhsu0268@gmail.com>
2016-08-26 17:35:52 -0400

    Fixed problem with too many calls to scope.isLoggedIn

--- public/javascripts/composer.js
+++ public/javascripts/composer.js
@@ -134,18 +134,23 @@ app.controller("NavCtrl", ['$scope', 'auth', function($scope, auth) {
 
     //$scope.isLoggedIn = auth.isLoggedIn;
 
+    var loggedIn;
+
 
     //$scope.isLoggedIn = auth.isLoggedIn;
-    //$scope.currentUser = auth.currentUser;
-    ///$scope.logout = auth.logOut;
-    isLoggedIn(auth);
+    $scope.currentUser = auth.currentUser;
+    $scope.logout = auth.logOut;
 
+    if (auth.isLoggedIn)
+    {
+        $scope.isLoggedIn = function()
+        {
+            return true;
+        }
+    }
 }]);
 
-function isLoggedIn(auth)
-{
-    auth.isLoggedIn;
-}
+
 
 app.controller('padsCtrl', ['$scope', function($scope) {
 
