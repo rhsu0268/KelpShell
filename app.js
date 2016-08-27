e640ac21600532af83ded227d1f0a2f1dad879f9
Richard Hsu <rhsu0268@gmail.com>
2016-08-27 19:47:00 -0400

    Successfully pulled in uploading images

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var multer = require('multer');
+//var multer = require('multer');
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/kelpshell');
@@ -17,7 +17,6 @@ var users = require('./routes/users');
 var mongoose = require('mongoose');
 var passport = require('passport');
 
-//var conn = mongoose.connection;
 
 
 
@@ -86,31 +85,7 @@ app.use(function(req, res, next) { //allow cross origin requests
         next();
 });
 
-// set up multer
-var storage = multer.diskStorage({ //multers disk storage settings
-    destination: function (req, file, cb) {
-        cb(null, './uploads/');
-    },
-    filename: function (req, file, cb) {
-        var datetimestamp = Date.now();
-        cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]);
-    }
-});
 
-var upload = multer({ //multer settings
-                storage: storage
-            }).single('file');
-
-/** API path that will upload the files */
-app.post('/upload', function(req, res) {
-    upload(req,res,function(err){
-        if(err){
-             res.json({error_code:1,err_desc:err});
-             return;
-        }
-         res.json({error_code:0,err_desc:null});
-    });
-});
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -122,8 +122,33 @@ router.get('/users', function(req, res, next) {
 
 });
 
+var multer = require('multer');
 
+// set up multer
+var storage = multer.diskStorage({ //multers disk storage settings
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        var datetimestamp = Date.now();
+        cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]);
+    }
+});
 
+var upload = multer({ //multer settings
+                storage: storage
+            }).single('file');
+
+/** API path that will upload the files */
+router.post('/upload', function(req, res) {
+    upload(req,res,function(err){
+        if(err){
+             res.json({error_code:1,err_desc:err});
+             return;
+        }
+         res.json({error_code:0,err_desc:null});
+    });
+});
 
 
 module.exports = router;
