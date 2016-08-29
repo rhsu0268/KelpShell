1061cdb2a0b740a2f57063ce37993db4b640e01a
Richard Hsu <rhsu0268@gmail.com>
2016-08-29 19:58:23 -0400

    Refactored auth code

--- public/javascripts/composer/composer.js
+++ public/javascripts/composer/composer.js
@@ -2,93 +2,11 @@
 
 var app = angular.module('composer', ['rzModule', 'ngFileUpload']);
 
-app.factory('auth', ['$http', '$window', function($http, $window) {
-
-    var auth = {};
-
-    auth.saveToken = function(token)
-    {
-        $window.localStorage['kelpshell-token'] = token;
-    }
-
-    auth.getToken = function()
-    {
-        return $window.localStorage['kelpshell-token'];
-    }
-
-    auth.isLoggedIn = function()
-    {
-        var token = auth.getToken();
-
-        if (token)
-        {
-            var payload = JSON.parse($window.atob(token.split('.')[1]));
-            return payload.exp = Date.now() / 1000;
-        }
-        else
-        {
-            return false;
-        }
-
-    }
-
-    auth.currentUser = function()
-    {
-        if (auth.isLoggedIn())
-        {
-            var token = auth.getToken();
-            var payload = JSON.parse($window.atob(token.split('.')[1]));
-
-            return payload.username;
-        }
-    }
-
-    auth.register = function(user)
-    {
-        return $http.post('/register', user).success(function(data) {
-
-            auth.saveToken(data.token);
-
-        })
-    }
-
-
-    auth.login = function(user)
-    {
-        return $http.post('/login', user).success(function(data) {
-
-            auth.saveToken(data.token);
-
-        })
-    }
-
-    auth.logOut = function()
-    {
-        $window.localStorage.removeItem('kelpshell-token');
-        $window.location.href = '/';
-    }
-
-    return auth;
-
-}]);
 
 
 
-app.controller("NavCtrl", ['$scope', 'auth', function($scope, auth) {
 
-    /*
-    $scope.isLoggedIn = function()
-    {
-        auth.isLoggedIn;
-    }
-    */
 
-    /*
-    $scope.currentUser = auth.currentUser;
-    $scope.logout = auth.logOut;
-    */
-
-}]);
 
 app.controller('uploadCtrl', ['Upload', '$window', function(Upload, $window) {
 
