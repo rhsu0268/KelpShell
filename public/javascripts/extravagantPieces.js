2bfaabd04d6e24f1fef7b0bb6d747cd79ad9d490
Richard Hsu <rhsu0268@gmail.com>
2016-09-10 12:00:20 -0400

    Added playing/stopping function to shared pieces

--- public/javascripts/extravagantPieces.js
+++ public/javascripts/extravagantPieces.js
@@ -154,6 +154,7 @@ app.controller('extravagantPiecesCtrl', ['$scope', 'auth', 'userPiece', '$fireba
 
     $scope.playPiece = function(fileName)
     {
+        console.log(fileName);
         context = new AudioContext();
         bufferLoader = new BufferLoader(
             context,
@@ -213,21 +214,6 @@ app.controller('sharePiecesCtrl', ['$scope', '$firebaseArray', '$rootScope', fun
 
 
 
-	$scope.sharePiece = function()
-	{
-		//pieces.submitPiece();
-		console.log($scope.title);
-		console.log($scope.composer);
-
-		$scope.masterpieces.$add({
-			title: $scope.title,
-			composer: $scope.composer
-		});
-
-		$scope.title = "";
-		$scope.composer = "";
-	};
-
     $rootScope.$on('sharePiece', function(event, data) {
 
         console.log(data);
@@ -236,6 +222,37 @@ app.controller('sharePiecesCtrl', ['$scope', '$firebaseArray', '$rootScope', fun
 			composer: data.composer
 		});
     });
+
+    $scope.playPiece = function(fileName)
+    {
+        context = new AudioContext();
+        bufferLoader = new BufferLoader(
+            context,
+            [
+               '../uploads/' + fileName
+            ],
+            finishedLoading	// this is the callback function - it's called after the file is loaded
+                            // and is given an array of loaded buffer arrays as an argument
+        );
+
+        bufferLoader.load();
+    }
+
+    function finishedLoading(bufferList) {
+        // If you had more loops, you could
+        source = context.createBufferSource();
+        source.buffer = bufferList[0];
+        gain = context.createGain();
+        source.connect(gain);
+        gain.gain.value = 0.4;
+        gain.connect(context.destination);
+        source.start(0);
+    }
+
+    $scope.stopPiece = function()
+    {
+        source.stop();
+    }
 
 
 }]);
